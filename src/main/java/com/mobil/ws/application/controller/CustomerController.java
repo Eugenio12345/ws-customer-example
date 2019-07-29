9c959656985ff215c52deebe0d3c7189d660fed8
Eugenio12345 <eugeniomerino79@gmail.com>
2019-07-29 17:35:22 -0500

    first rest message

--- src/main/java/com/mobil/ws/application/controller/CustomerController.java
+++ src/main/java/com/mobil/ws/application/controller/CustomerController.java
@@ -1,11 +1,14 @@
 package com.mobil.ws.application.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value="/v1/api")
+@RestController(value="/v1/mobile/customer")
 public class CustomerController {
 
-	
-	
+	@RequestMapping(value="/consulta")
+	public String getMessage(){
+		return "hello world";
+	}
 	
 }
