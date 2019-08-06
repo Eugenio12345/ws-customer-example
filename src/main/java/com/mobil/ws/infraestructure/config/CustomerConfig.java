390062560e0847abf868ac18c82c491ba12959e5
Eugenio12345 <eugeniomerino79@gmail.com>
2019-08-06 17:25:38 -0500

    add application.properties

--- src/main/java/com/mobil/ws/infraestructure/config/CustomerConfig.java
+++ src/main/java/com/mobil/ws/infraestructure/config/CustomerConfig.java
@@ -1,20 +1,6 @@
 package com.mobil.ws.infraestructure.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-@Configuration
 public class CustomerConfig {
-
-	@Bean(name = "dataSource")
-	public DriverManagerDataSource dataSource() {
-		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-		driverManagerDataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-		driverManagerDataSource.setUrl("jdbc:oracle:thin:@172.21.50.10:60135:ADBOBRBA");
-		driverManagerDataSource.setUsername("PROCCACUC1");
-		driverManagerDataSource.setPassword("Meve_123");
-		return driverManagerDataSource;
-	}
+	
 
 }
